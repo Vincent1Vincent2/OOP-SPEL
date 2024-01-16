caa39abe99af580a5335064900ca392590a9de0d
linusnilssson <linusonilsson@hotmail.com>
2024-01-16 17:17:47 +0100

    Update game.ts
    test

--- src/game.ts
+++ src/game.ts
@@ -2,17 +2,18 @@ class Game {
   private player: Player;
 
   constructor() {
-    this.player = new Player(
-      200,
-      200,
-      {
-        up: 87, // w
-        left: 65, // a
-        down: 83, // s
-        right: 68, // d
-      },
-      "red",
-    );
+
+    this.carOne = new Car(100, 300, "Blue");
+    this.carTwo = new Car(100, 500, "Red");
+    this.carOne.start(-2.3);
+    this.carTwo.start(-3);
+
+    this.playerOne = new Player("white", 900, 1000, {
+      up: 87, // w
+      left: 65, // a
+      down: 83, // s
+      right: 68, // d
+    });
   }
 
   public update() {
